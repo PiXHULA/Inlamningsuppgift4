58b95d0b5973e3602e7e44ae73f2cbdef19e9a0b
Joakim Briceno <joakim.onnhage@gmail.com>
2019-11-11 23:54:33 +0100

    Cleaning + added run() Methods and css sheet.

--- src/Gameview.java
+++ src/Gameview.java
@@ -1,13 +1,12 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Gameview extends Application implements Runnable{
@@ -15,6 +14,13 @@ public class Gameview extends Application implements Runnable{
     Button button2;
     Button button3;
     Button button4;
+    GridPane bottomPane;
+    GridPane buttonLayout;
+    BorderPane gameView;
+    TextArea quizArea;
+    TextArea onlineStatus;
+    TextArea highscoreArea;
+    HBox displayPlayers;
 
     public static void main(String[] args) {
         launch(args);
@@ -22,41 +28,86 @@ public class Gameview extends Application implements Runnable{
 
     @Override
     public void start(Stage stage) throws Exception {
-        TextArea quizArea = new TextArea();
-        quizArea.setEditable(false);
-        quizArea.setFocusTraversable(false);
-        quizArea.setWrapText(true);
-        button = createButton("Answer1");
-        button2 = createButton("Answer2");
-        button3 = createButton("Answer3");
-        button4 = createButton("Answer4");
+        VBox vert1 = new VBox();
+        VBox vert2 = new VBox();
+        VBox vert3 = new VBox();
+        vert1.setMinSize(200,600);
+        vert2.setMinSize(400,600);
+        vert3.setMinSize(200,600);
+        bottomPane = createBottomPane(vert1, vert2, vert3);
 
-        button.setOnAction(getActionEventEventHandler(quizArea, button.getText()));
-        button2.setOnAction(getActionEventEventHandler(quizArea, button2.getText()));
-        button3.setOnAction(getActionEventEventHandler(quizArea, button3.getText()));
-        button4.setOnAction(getActionEventEventHandler(quizArea, button4.getText()));
+        quizArea = createTextArea();
+        onlineStatus = createTextArea("Online: ","onlineStatus");
+        highscoreArea = createTextArea("Highscore: ", "highscoreArea");
 
-        BorderPane borderPane = new BorderPane();
-        borderPane.setPrefSize(400,600);
-        Label player1 = new Label("Player 1");
-        player1.setMinSize(5,5);
-        Label player2 = new Label("Player 2");
-        HBox playerStatus = new HBox(10,player1,player2);
-        playerStatus.setSpacing(400);
-        playerStatus.setPrefHeight(50);
-        GridPane buttonLayout = createButtonLayout();
-        borderPane.setTop(playerStatus);
-        borderPane.setCenter(quizArea);
-        borderPane.setBottom(buttonLayout);
-        Scene scene = new Scene(borderPane);
+        button = createButton("Answer1", quizArea);
+        button2 = createButton("Answer2", quizArea);
+        button3 = createButton("Answer3", quizArea);
+        button4 = createButton("Answer4", quizArea);
+
+        displayPlayers = displayNames("Player1", "Player2");
+        buttonLayout = createButtonLayout();
+        gameView = gameviewPane(quizArea, displayPlayers, buttonLayout);
+        vert1.getChildren().add(onlineStatus);
+        vert2.getChildren().add(gameView);
+        vert3.getChildren().add(highscoreArea);
+        Scene scene = new Scene(bottomPane);
+        scene.getStylesheets().add("GameviewStyle.css");
         stage.setScene(scene);
         stage.show();
     }
-    private Button createButton(String s){
-        Button button = new Button(s);
-        button.setMinSize(300.0,150.0);
+
+    private GridPane createBottomPane(VBox vert1, VBox vert2, VBox vert3) {
+        GridPane collectedGui = new GridPane();
+        collectedGui.setMinSize(800,600);
+        collectedGui.add(vert1,0,0);
+        collectedGui.add(vert2,1,0);
+        collectedGui.add(vert3,2,0);
+        return collectedGui;
+    }
+
+    private BorderPane gameviewPane(TextArea quizArea, HBox playerStatus, GridPane buttonLayout) {
+        BorderPane Gameview = new BorderPane();
+        Gameview.setPrefSize(400,600);
+        Gameview.setTop(playerStatus);
+        Gameview.setBottom(buttonLayout);
+        Gameview.setCenter(quizArea);
+        return Gameview;
+    }
+
+    private HBox displayNames(String player1, String player2) {
+        Label first = new Label(player1);
+        Label second = new Label(player2);
+        HBox playerStatus = new HBox(10,first,second);
+        playerStatus.setSpacing(200);
+        playerStatus.setAlignment(Pos.CENTER);
+        playerStatus.setPrefHeight(50);
+        return playerStatus;
+    }
+
+    private Button createButton(String answer, TextArea quizArea) {
+        Button button = new Button (answer);
+        button.setOnAction(getActionEventEventHandler(button, quizArea, button.getText()));
         return button;
     }
+
+    private TextArea createTextArea() {
+        TextArea textArea = new TextArea();
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+        textArea.setFocusTraversable(false);
+        return textArea;
+    }
+    private TextArea createTextArea(String label, String cssStyle) {
+        TextArea textArea = new TextArea();
+        textArea.setText(label);
+        textArea.setId(cssStyle);
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+        textArea.setFocusTraversable(false);
+        return textArea;
+    }
+
     private GridPane createButtonLayout() {
         GridPane buttonLayout = new GridPane();
         buttonLayout.add(button,0,0);
@@ -66,8 +117,14 @@ public class Gameview extends Application implements Runnable{
         return buttonLayout;
     }
 
-    private EventHandler<ActionEvent> getActionEventEventHandler(TextArea quizArea, String s) {
-        return actionEvent -> quizArea.appendText("Pushed " + s + "\n");
+    private EventHandler<ActionEvent> getActionEventEventHandler(Button button, TextArea quizArea, String s) {
+        return actionEvent -> {
+            if("Answer1".equals(s)){
+                quizArea.appendText("Pushed " + s + "\n");
+                button.setId("correctAnswer");
+            } else
+                button.setId("wrongAnswer");
+        };
     }
 
     @Override
---
+++ src/LoginView.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginView extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        //login screen. Username input > transfer username to "player1 / player " in gameview.
+    }
+}
