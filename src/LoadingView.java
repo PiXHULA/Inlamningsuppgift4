44a6a60fe629d3f76f75d8e162a97bb121b6381c
Joakim Briceno <joakim.onnhagebriceno@yh.nackademin.se>
2019-11-14 00:05:51 +0100

    branching

--- src/LoadingView.java
+++ src/LoadingView.java
@@ -1,16 +1,38 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class LoadingView extends Application {
+import javax.swing.*;
 
+public class LoadingView extends Application {
+    double ii = 0;
+    ProgressBar pb;
     public void start(Stage stage) throws Exception {
+        EventHandler<ActionEvent> event = e -> {
+            // set progress to different level of progressbar
+            ii += 0.1;
+            pb.setProgress(ii);
+        };
         BorderPane loadingView = new BorderPane();
+        HBox progressBar = new HBox();
+        progressBar.setAlignment(Pos.CENTER);
+        progressBar.setMinSize(400,100);
         loadingView.setMinSize(400, 600);
         Label loadingText = new Label("Please wait for a player to join your game!");
+
+        ProgressBar pb = new ProgressBar();
+
+        pb.setPrefSize(250,75);
+        progressBar.getChildren().add(pb);
         loadingView.setCenter(loadingText);
+        loadingView.setBottom(progressBar);
 
         Scene scene = new Scene(loadingView);
         scene.getStylesheets().add("GameviewStyle.css");
