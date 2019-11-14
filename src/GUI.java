f570171a6d0acc00b71238a19c5ec9b010a1ed62
Wilhelm Hattenbach <wilhelm.hattenbach@yh.nackademin.se>
2019-11-14 14:18:18 +0100

    test

---
+++ src/GUI.java
@@ -0,0 +1,18 @@
+public class GUI {
+
+    public void loginGUI(){
+
+    }
+
+    public void waitGUI(){
+
+    }
+
+    public void gameGUI(){
+
+    }
+
+    public void finishedGUI(){
+
+    }
+}
